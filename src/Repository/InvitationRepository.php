2a9cd4adf77d2984740c8774c6dc9d1376036080
baselrabia <baselrabia@gmail.com>
2022-09-29 03:49:45 +0200

    refactor invitation repository

--- src/Repository/InvitationRepository.php
+++ src/Repository/InvitationRepository.php
@@ -66,34 +66,6 @@ class InvitationRepository extends ServiceEntityRepository
         }
     }
 
-    // /**
-    //  * @return Invitation[] Returns an array of Invitation objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('i')
-            ->andWhere('i.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('i.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Invitation
-    {
-        return $this->createQueryBuilder('i')
-            ->andWhere('i.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
     /**
      * @throws OptimisticLockException
      * @throws ORMException
@@ -124,95 +96,38 @@ class InvitationRepository extends ServiceEntityRepository
      * @throws NonUniqueResultException
      * @throws AccessDeniedHttpException
      */
-    public function getEntity($user, $id): ?Invitation
+    public function getEntity($id): ?Invitation
     {
-        $entity = $this->createQueryBuilder('i')
+        return $this->createQueryBuilder('i')
             ->andWhere('i.id = :val')
             ->setParameter('val', $id)
             ->getQuery()
             ->getOneOrNullResult();
-
-        if ($entity) {
-            $sender = $entity->getSender()->getEmail();
-            $invited = $entity->getInvited()->getEmail();
-            $authorize = $user->getEmail() == $sender || $user->getEmail() == $invited;
-            if ($authorize) {
-                return $entity;
-            }
-            throw new AccessDeniedHttpException("UnAuthorized access to the invitation " . $id);
-        }
-        return null;
     }
 
     /**
      * @throws NonUniqueResultException
      */
-    public function cancel($user, $id): ?Invitation
+    public function cancel($entity): Invitation
     {
-        $entity = $this->getEntity($user, $id);
-
-        $this->Authorize($user, $entity, "cancel");
-
         $entity->setSenderStatus("cancel");
         $this->_em->flush();
-
         return $entity;
     }
 
-    public function accept($user, $id): ?Invitation
+    public function accept($entity): Invitation
     {
-        $entity = $this->getEntity($user, $id);
-
-        $this->Authorize($user, $entity, "accept");
-
         $entity->setInvitedStatus("accept");
         $this->_em->flush();
-
         return $entity;
     }
 
-    public function reject($user, $id): ?Invitation
+    public function reject($entity): Invitation
     {
-        $entity = $this->getEntity($user, $id);
-        $this->Authorize($user, $entity, "reject");
-
         $entity->setInvitedStatus("reject");
         $this->_em->flush();
-
         return $entity;
     }
 
-
-    private function Authorize($user, $entity, $action)
-    {
-        if (!$entity) {
-            throw new NotFoundHttpException("there is no entity");
-        }
-
-        $sender = $entity->getSender()->getEmail();
-        $senderStatus = $entity->getSenderStatus();
-        $invited = $entity->getInvited()->getEmail();
-        $currentUser = $user->getEmail();
-
-        $authorize = [
-            'cancel' => [
-                "value" => $currentUser == $sender,
-                "err_msg" => "UnAuthorized to Cancel the invitation "
-            ],
-            'accept' => [
-                "value" => $currentUser == $invited && $senderStatus == "send",
-                "err_msg" => "Invitation is canceled while trying to Accept the invitation "
-            ],
-            'reject' => [
-                "value" => $currentUser == $invited && $senderStatus == "send",
-                "err_msg" => "Invitation is canceled while trying to Reject the invitation "
-            ],
-        ];
-
-        if (!$authorize[$action]["value"]) {
-            throw new AccessDeniedHttpException($authorize[$action]["err_msg"]);
-        }
-
-    }
 
 }
--- src/Service/InvitationService.php
+++ src/Service/InvitationService.php
@@ -2,9 +2,15 @@
 
 namespace App\Service;
 
+use App\Entity\Invitation;
 use App\Repository\InvitationRepository;
 use App\Entity\User;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\NonUniqueResultException;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class InvitationService
 {
@@ -15,43 +21,109 @@ class InvitationService
         $this->invitationRepo = $invitationRepository;
     }
 
-    public function listSenderInvitations(User $user, string $status)
+    public function listSenderInvitations(User $user, string $status): Collection
     {
         return $this->invitationRepo->listSenderInvitations($user, $status);
     }
 
-    public function listInvitedInvitations(User $user, string $status)
+    public function listInvitedInvitations(User $user, string $status): Collection
     {
         return $this->invitationRepo->listInvitedInvitations($user, $status);
     }
 
-    public function sendInvitation(User $user, array $data)
+    /**
+     * @throws OptimisticLockException
+     * @throws ORMException
+     */
+    public function sendInvitation(User $user, array $data): Invitation
     {
         return $this->invitationRepo->create($user, $data);
-    }
-
-    public function getInvitation($user, $id)
-    {
-        return $this->invitationRepo->getEntity($user, $id);
     }
 
     /**
      * @throws NonUniqueResultException
      */
-    public function cancelInvitation($user, $id)
+    public function getInvitation($user, $id): ?Invitation
     {
-        return $this->invitationRepo->cancel($user, $id);
+       $entity =  $this->invitationRepo->getEntity($id);
+
+       $this->Authorize($user,"view", $entity);
+
+       return $entity;
     }
 
-    public function acceptInvitation($user, $id)
+    /**
+     * @throws NonUniqueResultException
+     */
+    public function cancelInvitation($user, $id): ?Invitation
     {
-        return $this->invitationRepo->accept($user, $id);
+        $entity = $this->invitationRepo->getEntity($id);
+
+        $this->Authorize($user, "cancel", $entity);
+
+        return $this->invitationRepo->cancel($entity);
     }
 
-    public function rejectInvitation($user, $id)
-    {
-        return $this->invitationRepo->reject($user, $id);
 
+    /**
+     * @throws NonUniqueResultException
+     */
+    public function acceptInvitation($user, $id): ?Invitation
+    {
+        $entity = $this->invitationRepo->getEntity($id);
+
+        $this->Authorize($user, "accept", $entity);
+
+        return $this->invitationRepo->accept($entity);
     }
 
+
+    /**
+     * @throws NonUniqueResultException
+     */
+    public function rejectInvitation($user, $id): ?Invitation
+    {
+        $entity = $this->invitationRepo->getEntity($id);
+
+        $this->Authorize($user, "reject", $entity);
+
+        return $this->invitationRepo->reject($entity);
+    }
+
+
+    private function Authorize($user, $action, $entity): void
+    {
+        if (!$entity) {
+            throw new NotFoundHttpException("there is no entity");
+        }
+
+        $sender = $entity->getSender()->getEmail();
+        $senderStatus = $entity->getSenderStatus();
+        $invited = $entity->getInvited()->getEmail();
+        $currentUser = $user->getEmail();
+
+        $authorize = [
+            'view' => [
+                "value" => $currentUser == $sender || $currentUser == $invited,
+                "err_msg" => "UnAuthorized access to view this invitation"
+            ],
+            'cancel' => [
+                "value" => $currentUser == $sender,
+                "err_msg" => "UnAuthorized to Cancel the invitation"
+            ],
+            'accept' => [
+                "value" => $currentUser == $invited && $senderStatus == "send",
+                "err_msg" => "Invitation is canceled while trying to Accept the invitation"
+            ],
+            'reject' => [
+                "value" => $currentUser == $invited && $senderStatus == "send",
+                "err_msg" => "Invitation is canceled while trying to Reject the invitation"
+            ],
+        ];
+
+        if (!$authorize[$action]["value"]) {
+            throw new AccessDeniedHttpException($authorize[$action]["err_msg"]);
+        }
+
+    }
 }
